d2341e9742fe225152576de2c8cc7d5a09266947
tinchoz8426 <76183907+tinchoz8426@users.noreply.github.com>
2021-09-21 10:52:14 -0300

    cambios 21-09

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 let scrollBoton = document.querySelector(".arrow-icon"); //Variable que apunta al boton de subir al top
-let modalBoton = document.getElementById("btn-open-modal");
-let closeBoton = document.getElementById("close-button");
-let modalCustom = document.getElementById("modal__custom");
-
-
+let enviarCv = document.getElementById("botoncitoPaProbrar");
+let agregarClase = document.querySelector(".modal-footer__trabaja-nosotros");
+let eliminarBody = document.querySelector(".modal-body__trabaja-nosotros");
+let inputName = document.getElementById("inputName");
+let inputTel = document.getElementById("inputTel");
 
 //Funcion de subir al top con boton
 addEventListener('scroll', () => {
@@ -17,12 +17,43 @@ scrollBoton.addEventListener('click', () => {
 })
 
 
-//prueba modal
-/* modalBoton.addEventListener('click', (e) => {
+
+//probando el botoncito
+
+const form = document.getElementById("formito");
+
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    modalCustom.classList.add("show-modal");
+    agregarClase.style.display = 'flex';
+    eliminarBody.style.display = 'none';
+    form.submit(() => {
+        setTimeout(() => {
+
+        }, 10000);
+        return true;
+    });
+
 })
-closeBoton.addEventListener('click', (e) => {
+
+/* enviarCv.addEventListener('submit', (e) => {
     e.preventDefault();
-    modalCustom.classList.remove("show-modal");
+    agregarClase.style.display = 'flex';
+    eliminarBody.style.display = 'none';
+    setTimeout(function () {
+        fetch("https://formsubmit.co/ajax/your@email.com", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                name: "FormSubmit",
+                message: "I'm from Devro LABS"
+            })
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error));
+        // window.location = "nosotros.html";
+    }, 4000);
 }) */
